505fcf73d84aa02efec14db1eb3bc2ad164be3c8
zackluckyf <fanning.34@gmail.com>
2016-12-04 21:02:54 -0500

    Git integrated into slack and example component created

--- src/app/example-sub-module/example.module.ts
+++ src/app/example-sub-module/example.module.ts
@@ -14,3 +14,4 @@ export class ExampleModule { } // export module
 // I used the example component on line 42 of app.component.html
 // The current structure is app.module.ts -> example.module.ts -> example.component.ts
 // Make sure to export your component from any sub module you want to use
+// Used TDD to make the example component
