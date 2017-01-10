b15a50f9fc7ae1a61c8083c9dc8fe1a882e6a85b
Jory Schmidt <joryschmidt1988@gmail.com>
2017-01-10 23:17:46 +0000

    add exponentiation, square root, and modulus

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -11,11 +11,11 @@ export class CalculatorComponent implements OnInit {
     username: string = '';
     name: string = '';
     calc_keys: Array<string[]> = [
-        ['7','8','9','/'],
-        ['4','5','6','*'],
-        ['1','2','3','-'],
-        ['0','.','=','+'],
-        ['(',')','C','^']
+        ['7','8','9','/', ','],
+        ['4','5','6','*', '1/x'],
+        ['1','2','3','-', 'exp'],
+        ['0','.','=','+', 'sqrt'],
+        ['(',')','C','%', 'Back']
     ];
 
     constructor() { }
@@ -32,16 +32,39 @@ export class CalculatorComponent implements OnInit {
     }
 
     eval():void {
-        this.display = eval(this.display);
+        this.display = eval(this.display).toString();
     }
     
     clear():void {
         this.display = '';
     }
     
+    exponent():void {
+        let dsp = this.display;
+        let base = /\d+$/.exec(dsp);
+        dsp = dsp.slice(0, base.index);
+        this.display = dsp + 'Math.pow(' + base[0] + ',';
+    }
+    
+    sqrt():void {
+        let dsp = this.display;
+        let base = /\d+$/.exec(dsp);
+        dsp = dsp.slice(0, base.index);
+        // Will automatically perform the operation if the number is the only thing in display
+        if (!dsp) this.display = Math.sqrt(Number(base[0])).toString();
+        else this.display = dsp + 'Math.sqrt(' + base[0] + ')';
+    }
+    
+    backspace():void {
+        this.display = this.display.slice(0, this.display.length - 1);
+    }
+    
     determineFunction(key) {
         if (key === '=') return this.eval();
         if (key === 'C') return this.clear();
+        if (key === 'exp') return this.exponent();
+        if (key === 'sqrt') return this.sqrt();
+        if (key === 'Back') return this.backspace();
         else return this.input(key);
     }
 }
